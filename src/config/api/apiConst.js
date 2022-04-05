68671f5d775305dd56ec34ba39a25e81c04f9ab3
Vũ Xuân Hoàng <hoangvx@vtconline.com>
2022-04-05 21:35:54 +0700

    1

--- src/config/api/apiConst.js
+++ src/config/api/apiConst.js
@@ -1,9 +1,9 @@
 // const baseUrl = process.env.REACT_APP_BASE_API_URL;
 
 // const baseUrl = "https://english-app-backend.herokuapp.com";
-// const baseUrl = "https://english-backend-v2.herokuapp.com";
+const baseUrl = "https://english-backend-v2.herokuapp.com";
 
-const baseUrl = "http://localhost:8888";
+// const baseUrl = "http://localhost:8888";
 
 export const ApiUrl = {
   questions: baseUrl + "/questions",
